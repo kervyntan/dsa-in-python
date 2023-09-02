01d4dbad06f2fffa98777e68f67cc6c3ff53020f
Kervyn <73232587+kervyntan@users.noreply.github.com>
2023-09-02 19:08:25 +0800

    update linkedlist push

--- stack/stack_linkedlist.py
+++ stack/stack_linkedlist.py
@@ -3,4 +3,18 @@ class Node:
         self.data = data
         self.next = None
 
-    
+class Stack:
+    def __init__(self):
+        self.top = None
+        self.size = 0
+
+    def push(self, data):
+        node_to_be_added = Node(data)
+        if self.size == 0:
+            self.top = node_to_be_added
+            # node_to_be_added.next = None
+        else:
+            node_to_be_added.next = self.top
+            self.top = node_to_be_added
+        self.size += 1
+        print(node_to_be_added.data)
